e19b67d59cf17846315848e06a49a414a54c4291
luch34 <luchcollins@gmail.com>
2019-08-18 11:51:24 +0300

    Add days and their strings on javascripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,14 @@ function culculationOfDay(x) {
     period[x] = o;
 }
 // new age culculating arraymaker
+
+days= culculationOfDay(7);
+days= [sunday, monday, tuesday, wednesday, thursday, friday,sartuday]
+
+day[1] = "sunday"
+day[2] = "monday"
+day[3] ="tuesday"
+da3[4] = "wednesday"
+day[5] = "thursday"
+day[6] = "friday"
+day[7] = "saturday"
