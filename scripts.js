89581a8f842d48b189e44ba5ec81a041d1fa2f59
luch34 <luchcollins@gmail.com>
2019-08-18 12:00:53 +0300

    Add the fomulae on scripts.js to culculate birthday and akan names

--- scripts.js
+++ scripts.js
@@ -50,3 +50,5 @@ function compute(form) {
     var x =parseInt(form.year.value)
     if (x>o) & (X>7) {
       alert("invalid)
+
+      Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
