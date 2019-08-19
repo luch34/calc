9008d80bee5346627dbe53cdccb5ccc68bf35413
luch34 <luchcollins@gmail.com>
2019-08-19 10:52:48 +0300

    Add scripts.js inside js directory and add a link to index.html to link with js

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+function culculationOfDay(x) {
+ period = x
+ for(var x = 1 , 1 < x,   x++ )
+    period[x] = o;
+}
+// new age culculating arraymaker
+
+days= culculationOfDay(7);
+days= [sunday, monday, tuesday, wednesday, thursday, friday,sartuday]
+
+day[1] = "sunday"
+day[2] = "monday"
+day[3] ="tuesday"
+da3[4] = "wednesday"
+day[5] = "thursday"
+day[6] = "friday"
+day[7] = "saturday"
+
+maleNames= culculationOfDay(7);
+maleNames= [kwasi, kwando, kwabena, kwaku, yaw, kwame]
+maleNames[1] = "kwasi"
+malenames[2] = "kwando"
+malenames[3] = "kwabena"
+malenames[4] = "kwaku"
+malenames[5] = "yaw"
+malenames[6] = "kwame"
+
+femaleNames= culculationOfDay(7);
+femaleNames[1]= [akosua, adowa, abennaa, akua, yaa, afua, ama]
+femaleNames[2]= "akosua"
+femaleNames[3]= "adowa"
+femaleNames[4]= "akua"
+femaleNames[5]= "yaa"
+femaleNames[6]= "afua"
+femaleNames[7]= "ama"
+
+
+function compute(form) {
+  var x = parseInt(form.day.value)
+  if (x<0) & (x > 7) {
+    alert("Invalid")
+  }
+  function compute(form) {
+    var x =parseInt(form.day.value)
+    if (x<o) & (x>7) {
+      alert("invalid")
+  }
+
+  function functionName(form) {
+    var x =parseInt(form.year.value)
+    if (x>o) & (X>7) {
+      alert("invalid)
+
+      Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
